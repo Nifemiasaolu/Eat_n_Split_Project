f2d2af4d29300886edec9e40ed3b467a6a7a213c
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-09-04 22:06:17 +0100

    Feat: Compiled functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,28 @@ import Button from "./Button";
 import FormSplitBill from "./FormSplitBill";
 import { useState } from "react";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+
 export default function App() {
   const [friends, setFriends] = useState(initialFriends)
   const [showAddFriend, setShowAddFriend] = useState(false);
